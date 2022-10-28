ec03fdd24d68b947d4136efc4e2e2e3fc167a49b
Kevin Gomes <kgomes.mazzatech@ext.valid.com>
2022-10-27 23:38:18 -0400

    spring-liquibase-xml init

---
+++ spring-liquibase-xml/src/main/java/com/example/springliquibasexml/SpringLiquibaseXmlApplication.java
@@ -0,0 +1,13 @@
+package com.example.springliquibasexml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLiquibaseXmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLiquibaseXmlApplication.class, args);
+	}
+
+}
---
+++ spring-liquibase-xml/src/test/java/com/example/springliquibasexml/SpringLiquibaseXmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springliquibasexml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLiquibaseXmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
